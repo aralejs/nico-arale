fd67743b6a5e5580df4b2f7057ce7d4706132124
popomore <sakura9515@gmail.com>
2012-11-07 01:12:31 +0800

    for travis

---
+++ test-travis.js
@@ -0,0 +1,40 @@
+var fs = require('fs');
+var http = require('http');
+var static = require('node-static');
+var swig = require('swig');
+
+swig.init({
+    autoescape: false,
+    filters: {
+        json_dumps: function(input) {
+            console.log(JSON.stringify(input))
+            return JSON.stringify(input);
+        }
+    }
+});
+var package = require(fs.realpathSync('package.json'))
+var tmpl = swig.compileFile(fs.realpathSync('tests/templates/runner.html'));
+var result = tmpl.render({
+    static_url: function(url) {
+        return './static/' + url;
+    },
+    resource: {
+        package: package
+    },
+    site: {
+        name: package.name
+    },
+    theme: {
+        debug: true
+    }
+});
+fs.writeFileSync(fs.realpathSync('tests/runner.html'), result);
+
+// server
+var file = new static.Server(fs.realpathSync('.'));
+http.createServer(function(request, response) {
+    request.addListener('end', function() {
+        file.serve(request, response);
+    });
+}).listen(8000);
+
