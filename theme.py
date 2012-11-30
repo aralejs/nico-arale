3c3a84757f82b7dfa8f494236c01ee6ce8290aa4
popomore <sakura9515@gmail.com>
2012-11-30 10:28:36 +0800

    fix arale2 theme link

--- theme.py
+++ theme.py
@@ -3,7 +3,7 @@
 
 name = 'Arale2'
 version = '0.1'
-website = 'https://github.com/alipay/liquidluck-theme-arale2'
+website = 'https://github.com/aralejs/liquidluck-theme-arale2'
 author = {
     'name': 'Hsiaoming Yang',
     'website': 'http://aralejs.org',
