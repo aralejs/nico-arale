10fa82bd7e563fe93a219c5f5c413891266cf27a
Hsiaoming Yang <lepture@me.com>
2012-12-11 19:32:00 +0800

    save theme

--- arale.js
+++ arale.js
@@ -1,15 +1,64 @@
 var path = require('path')
+var fs = require('fs')
 
-var writer = '/Users/lepture/workspace/node/nico/lib/writer.'
+var nico = '/Users/lepture/workspace/node/nico'
+var Post = require(nico).Post
 
+Object.defineProperty(Post.prototype, 'template', {
+  configurable: true,
+  get: function() {
+    if (this.relative_filepath == 'HISTORY.md') return 'history.html';
+    return 'post.html';
+  }
+});
+
+Object.defineProperty(Post.prototype, 'filename', {
+  configurable: true,
+  get: function() {
+    if (this.relative_filepath == 'README.md') return 'index.md';
+    if (this.meta.filename) return this.meta.filename;
+    var basename = path.basename(this.relative_filepath);
+    return basename.split('.')[0];
+  }
+});
+
+Object.defineProperty(Post.prototype, 'category', {
+  configurable: true,
+  get: function() {
+    if (this.relative_filepath == 'README.md') return 'docs';
+    if (this.meta.category) return this.meta.category;
+    return this.directory;
+  }
+});
+
+
+
+// {{ settings for nico
 exports.theme = __dirname
-exports.package = require(path.join(process.cwd(), 'package.json'))
 exports.source = process.cwd()
 exports.output = path.join(process.cwd(), '_site')
 exports.permalink = '{{directory}}/{{filename}}.html'
 exports.ignore = ['_site']
 exports.writers = [
-  writer + 'PageWriter',
-  writer + 'StaticWriter',
-  writer + 'FileWriter',
+  nico + '.PageWriter',
+  nico + '.StaticWriter',
+  nico + '.FileWriter',
+  path.join(__dirname, 'theme') + '.MochaWriter'
 ]
+exports.PostRender = Post
+// end settings }}
+
+
+// extends for theme usage, that can be accessable by {{config.xxx}}
+exports.package = require(path.join(process.cwd(), 'package.json'))
+
+if (fs.existsSync(path.join(process.cwd(), 'HISTORY.md'))) {
+  exports.hasHistory = true
+} else {
+  exports.hasHistory = false
+}
+if (fs.existsSync(path.join(process.cwd(), 'tests'))) {
+  exports.hasTest = true
+} else {
+  exports.hasTest = false
+}
--- theme.js
+++ theme.js
@@ -1,8 +1,32 @@
 #!/usr/bin/env node
 
 var path = require('path')
+var writer = require('/Users/lepture/workspace/node/nico/lib/writer')
 
 
 exports.name = 'arale'
 exports.version = '0.1'
 exports.lang = 'zh'
+
+exports.MochaWriter = writer.BaseWriter.extend({
+  writerName: 'MochaWriter',
+
+  run: function() {
+    this.render({
+      destination: path.join(this.storage.config.output, 'tests', 'runner.html'),
+      template: 'mocha-runner.html'
+    });
+  }
+});
+
+
+exports.JasmineWriter = writer.BaseWriter.extend({
+  writerName: 'JasmineWriter',
+
+  run: function() {
+    this.render({
+      destination: path.join(this.storage.config.output, 'tests', 'runner.html'),
+      template: 'jasmine-runner.html'
+    });
+  }
+});
