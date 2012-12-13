c3d3f7b65c31840b22d7baccfa17725764aa44dc
Hsiaoming Yang <lepture@me.com>
2012-12-13 14:08:46 +0800

    add bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+CWD=$(dirname "$0")
+THEME="$HOME/.spm/themes/arale"
+JSC="https://raw.github.com/lepture/nico/master/scripts/node-jscoverage.rb"
+
+cd $CWD
+
+if [ ! -d $THEME ]; then
+    echo "clone nico arale theme"
+    git clone https://github.com/aralejs/nico-arale.git $THEME
+else
+    echo "update nico arale theme"
+    cd $THEME
+    git pull origin master
+    cd $CWD
+fi
+
+if ! which jscoverage > /dev/null; then
+    echo "install jscoverage"
+    curl -s $JSC -o jscoverage.rb
+    brew install jscoverage.rb
+else
+    echo "you have installed jscoverage"
+fi
+
+if ! which jade > /dev/null; then
+    echo "install jade"
+    npm install jade -g
+else
+    echo "you have installed jade"
+fi
+
+if ! which mocha > /dev/null; then
+    echo "install mocha"
+    npm install mocha -g
+else
+    echo "you have installed mocha"
+fi
+
+
+if ! which mocha-phantomjs > /dev/null; then
+    echo "install mocha phantomjs"
+    npm install mocha-phantomjs -g
+else
+    echo "you have installed mocha-phantomjs"
+fi
