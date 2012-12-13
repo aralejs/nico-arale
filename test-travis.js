2e28b06169c7a73586574781a85dd87e12a67a5b
Hsiaoming Yang <lepture@me.com>
2012-12-13 22:13:45 +0800

    use the before one

--- test-travis.js
+++ test-travis.js
@@ -1,9 +1,35 @@
 var fs = require('fs');
 var http = require('http');
 var static = require('node-static');
+var swig = require('swig');
+
+swig.init({
+    autoescape: false
+});
+
+var package = require(fs.realpathSync('package.json'));
+console.log('read package.json');
+
+var tmpl = swig.compileFile(fs.realpathSync('tests/templates/runner.html'));
+var result = tmpl.render({
+    static_url: function(url) {
+        return './static/' + url;
+    },
+    config: {
+        package: package
+    },
+    site: {
+        name: package.name
+    },
+    theme: {
+        debug: true
+    }
+});
+fs.writeFileSync('tests/runner.html', result);
+console.log('generate tests/runner.html');
 
 // Server Start
-var file = new static.Server(fs.realpathSync('_site'));
+var file = new static.Server(fs.realpathSync('.'));
 http.createServer(function(request, response) {
     request.addListener('end', function() {
         file.serve(request, response);
