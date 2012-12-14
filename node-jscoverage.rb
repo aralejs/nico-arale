8cacce11220ae07fefe6a06131e548004d7701ee
Hsiaoming Yang <lepture@me.com>
2012-12-14 15:43:30 +0800

    fix jscoverage

--- node-jscoverage.rb
+++ node-jscoverage.rb
@@ -5,11 +5,11 @@ class NodeJscoverage < Formula
   url 'git://github.com/visionmedia/node-jscoverage.git'
   sha1 ''
 
+  depends_on 'automake'
 
   def install
 
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}"
+    system "./configure", "--disable-debug", "--prefix=#{prefix}"
     system "make"
     system "make install"
 
