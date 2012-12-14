892aa9297714ef577ea806cf4db654ef0262aedd
Hsiaoming Yang <lepture@me.com>
2012-12-14 14:14:54 +0800

    use test suite

---
+++ update-static.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import urllib
+import subprocess
+
+MOCHA = "https://raw.github.com/visionmedia/mocha/%s/mocha.js" % "1.7.4"
+PUERH = "https://raw.github.com/popomore/puerh/%s/puerh.js" % "0.0.1"
+SINON = "http://sinonjs.org/releases/sinon-%s.js" % "1.5.2"
+
+
+def fetch(url, filename, replace=None):
+    print 'fetching: %s' % url
+    f = urllib.urlopen(url)
+    content = f.read()
+    f.close()
+    if replace:
+        content = replace(content)
+    f = open(filename, 'w')
+    f.write(content)
+    f.close()
+
+
+fetch(
+    MOCHA, 'tmp/mocha.js',
+    lambda o: o.replace('window.scrollTo', '// window.scrollTo')
+)
+fetch(
+    "https://raw.github.com/visionmedia/mocha/%s/mocha.css" % "1.7.4",
+    'static/css/mocha.css'
+)
+fetch(PUERH, 'tmp/puerh.js')
+fetch(SINON, 'tmp/sinon.js')
+
+subprocess.call(
+    [
+        'uglifyjs',
+        'tmp/mocha.js',
+        'tmp/sinon.js',
+        'tmp/puerh.js',
+        '-o', 'static/js/test-suite.js'
+    ]
+)
